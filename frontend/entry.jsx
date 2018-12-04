7a2dea83742e3ac36ef69997dd3c3c6d4349c500
Tyler <tylerthomasallen@gmail.com>
2018-12-03 16:35:18 -0800

    Create static_pages controller, connect React frontend to rails backend

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
 
     const root = document.getElementById('root');
-    ReactDOM.render(<Root />, root)
+    ReactDOM.render(<h1>Hello, World</h1>, root)
 });
 
